f2d4bc33bbb15a1c455806cc80ebc262d03017ae
Anurag Singh <anirock2050@gmail.com>
2023-09-23 21:22:59 +0530

    final-touch

--- gpt-frontend/src/Components/Chatbot.js
+++ gpt-frontend/src/Components/Chatbot.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./Chatbot.css"; // Import the CSS for styling
 
@@ -7,6 +7,12 @@ const API_BASE_URL = "http://localhost:5000";
 function Chatbot(props) {
   const [userInput, setUserInput] = useState("");
   const [conversation, setConversation] = useState([]);
+  
+  // Create a ref for the chat content element
+  const chatContentRef = useRef(null);
+
+  // Create a ref for the last assistant message element
+  const lastAssistantMessageRef = useRef(null);
 
   const handleUserInput = (event) => {
     setUserInput(event.target.value);
@@ -14,6 +20,13 @@ function Chatbot(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const sentMessage = { role: "user", content: userInput, hidden: false };
+
+    // Add the sent text to the conversation immediately
+    setConversation((prevConversation) => [...prevConversation, sentMessage]);
+
+    // Clear the input field
+    setUserInput("");
 
     const response = await axios.post(`${API_BASE_URL}/ask`, {
       user_input: userInput,
@@ -21,18 +34,34 @@ function Chatbot(props) {
 
     const assistantResponse = response.data.assistant_response;
 
-    setConversation([
-      ...conversation,
-      { role: "user", content: userInput },
-      { role: "assistant", content: assistantResponse },
+    // Update the sent message with the actual response
+    // sentMessage.content = assistantResponse;
+
+    // Update the conversation with the sent message and assistant's response
+    setConversation((prevConversation) => [
+      ...prevConversation,
+      { role: "assistant", content: assistantResponse, hidden: true },
     ]);
 
-    setUserInput("");
+    // Scroll to the last assistant message
+    lastAssistantMessageRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   const handleClearChat = () => {
     setConversation([]);
   };
+
+  // Use useEffect to trigger the fade-in effect
+  useEffect(() => {
+    const messages = document.querySelectorAll(".chat-bubble");
+
+    messages.forEach((message, index) => {
+      // Use a timeout to stagger the animations
+      setTimeout(() => {
+        message.style.opacity = 1; // Set opacity to 1 to trigger the fade-in effect
+      }, index * 100); // Adjust the delay as needed
+    });
+  }, [conversation]);
 
   useEffect(() => {
     // Greet the user when the chatbot component mounts
@@ -41,6 +70,7 @@ function Chatbot(props) {
       {
         role: "assistant",
         content: `Hello ${props.userName}, how can I help you?`,
+        hidden: true,
       },
     ]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -49,30 +79,38 @@ function Chatbot(props) {
   return (
     <div className="chat-container">
       <div className="chat-header">
-        <div className="chat-title">Chatbot</div><br></br>
+        <div className="chat-title">Chatbot</div>
         <div className="clear-button" onClick={handleClearChat}>
           Clear
         </div>
       </div>
-      <div className="chat-content">
-  {conversation.map((message, index) => (
-    <div
-      key={index}
-      className={`chat-bubble ${
-        message.role === "user" ? "user-bubble" : "assistant-bubble"
-      }`}
-    >
-      {message.role === "user" && (
-        <div className="message-label">User</div>
-      )}
-      {message.role === "assistant" && (
-        <div className="message-label">AI</div>
-      )}
-      {message.content}
-    </div>
-  ))}
-</div>
-
+      {/* Attach the ref to the chat content */}
+      <div ref={chatContentRef} className="chat-content">
+        {conversation.map((message, index) => (
+          <div
+            key={index}
+            className={`chat-bubble ${
+              message.role === "user" ? "user-bubble" : "assistant-bubble"
+            }`}
+            style={{ opacity: message.hidden ? 0 : 1 }} // Set opacity based on the 'hidden' property
+          >
+            {message.role === "user" && (
+              <div className="message-label">User</div>
+            )}
+            {message.role === "assistant" && (
+              <div
+                className="message-label"
+                ref={message.role === "assistant" ? lastAssistantMessageRef : null} // Set ref for the last assistant message
+              >
+                AI
+              </div>
+            )}
+            {message.content}
+          </div>
+        ))}
+        {/* Add an empty div with a ref to scroll to the last assistant message */}
+        <div ref={lastAssistantMessageRef}></div>
+      </div>
       <form onSubmit={handleSubmit} className="chat-input-container">
         <input
           type="text"
